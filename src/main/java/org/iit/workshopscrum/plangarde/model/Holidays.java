308841eeb97699469625902085193841f6684575
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2015-05-30 17:38:19 +0100

    update planning

--- src/main/java/org/iit/workshopscrum/plangarde/model/Holidays.java
+++ src/main/java/org/iit/workshopscrum/plangarde/model/Holidays.java
@@ -7,30 +7,31 @@ import java.util.Date;
 
 /**
  * Created by Mohamed Chams Eddin Mouedhen & Yahia Elhem on 30/05/2015.
- *
  */
+
 /**
-* <h1>Add day</h1>
-* The AddNum program implements an application that
-* simply add : given date.
-* the output on the screen.
-* <p>
-* <b>Note:</b> Giving proper comments in your program makes it more
-* user friendly and it is assumed as a high quality code.
-*
-* @author  Chams
-* @version 1.0
-* @since   2014-05-30
-*/
+ * <h1>Add day</h1>
+ * The AddNum program implements an application that
+ * simply add : given date.
+ * the output on the screen.
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author Chams
+ * @version 1.0
+ * @since 2014-05-30
+ */
 public class Holidays {
-	/**
-	   * This method is used to add holidays (dateBegin->dateend). This is
-	   * a the simplest form of a class method, just to
-	   * show the usage of various javadoc Tags.
-	   * @param dateBegin is the first param.
-	   *  @param dateBegin is the second param.
-	   * @return bouolean (true or false) .
-	   */
+    /**
+     * This method is used to add holidays (dateBegin->dateend). This is
+     * a the simplest form of a class method, just to
+     * show the usage of various javadoc Tags.
+     *
+     * @param dateBegin is the first param.
+     * @param dateBegin is the second param.
+     * @return bouolean (true or false) .
+     */
 
     Date dateBegin;
     Date dateEnd;
--- src/main/java/org/iit/workshopscrum/plangarde/model/Planning.java
+++ src/main/java/org/iit/workshopscrum/plangarde/model/Planning.java
@@ -10,29 +10,30 @@ import java.util.List;
 /**
  * Created by Mohamed Chams Eddin Mouedhen & Elhem Yahia on 30/05/2015.
  */
+
 /**
-* <h1>Add planing</h1>
-* The program implements an application that
-* simply add : given date.
-* the output on the screen.
-* <p>
-* <b>Note:</b> Giving proper comments in your program makes it more
-* user friendly and it is assumed as a high quality code.
-*
-* @author  Chams
-* @version 1.0
-* @since   2014-05-30
-*/
+ * <h1>Add planing</h1>
+ * The program implements an application that
+ * simply add : given date.
+ * the output on the screen.
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author Chams
+ * @version 1.0
+ * @since 2014-05-30
+ */
 public class Planning {
-	/**
-	   * This method is used to add planing. This is
-	   * a the simplest form of a class method, just to
-	   * show the usage of various javadoc Tags.
-	   * @param list of doctor
-	   * @param beginplan :type date
-	   * @param endplan   :type date
-	   
-	   */
+    /**
+     * This method is used to add planing. This is
+     * a the simplest form of a class method, just to
+     * show the usage of various javadoc Tags.
+     *
+     * @param list of doctor
+     * @param beginplan :type date
+     * @param endplan   :type date
+     */
 
     private List<Doctor> doctors;
     private Date beginPlan;
@@ -137,7 +138,8 @@ public class Planning {
 
         for (; calendarMin.before(calendarMax); calendarMin.add(Calendar.DATE, 1)) {
             for (Doctor doctor : this.doctors) {
-                if (doctor.getPreferences().contains(calendarMin.get(Calendar.DAY_OF_WEEK))) {
+                DayWeek d = DayWeek(calendarMin.get(Calendar.DAY_OF_WEEK));
+                if (doctor.getPreferences().contains() {
                     for (int j = 0; j < doctor.getHolidays().size(); j++) {
                         if (!doctor.getHolidays().get(j).checkDay(calendarMin.getTime())) {
                             this.addPlanDayUniqueDoctor(calendarMin.getTime(), doctor);
