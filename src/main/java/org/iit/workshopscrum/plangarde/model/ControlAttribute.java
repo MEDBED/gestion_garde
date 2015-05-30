08fd278c7dee0023d7bf68973e51a228d0b92caa
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2015-05-30 17:11:44 +0100

    delete obsolete class

---
+++ src/main/java/org/iit/workshopscrum/plangarde/model/ControlAttribute.java
@@ -0,0 +1,14 @@
+package org.iit.workshopscrum.plangarde.model;
+
+/**
+ * Created by Mohamed Chams Eddin Mouedhen & Elhem Yahia on 30/05/2015.
+ */
+public class ControlAttribute {
+
+    public static boolean isString(String stringToControl) {
+        return stringToControl.matches("\\p{Alpha}*");
+    }
+
+    public static boolean isNumeric(String in)
+
+}
